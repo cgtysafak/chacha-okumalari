e42bd2b2872fa138f3a7636543b633e989775dfd
cgtysafak <cagatay.safak@ug.bilkent.edu.tr>
2020-08-04 08:44:30 +0300

    SplashScreen
    I have designed a full-amateur logo and a splash screen. Now, all I have to do is adding advertisements to my application and release it on Google Play.

---
+++ app/src/main/java/com/cgty/okumalarimuser/SplashScreen.java
@@ -0,0 +1,33 @@
+package com.cgty.okumalarimuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreen extends AppCompatActivity
+{
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_splash_screen);
+		
+		new Handler().postDelayed(new Runnable()
+		{
+			@Override
+			public void run()
+			{
+				Intent intentSplash;
+				intentSplash = new Intent(SplashScreen.this, MainActivity.class);
+				
+				startActivity(intentSplash);
+				
+				finish();
+			}
+		},1250);
+	
+	}
+}
